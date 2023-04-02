731e8f1adb8c26a372d84512df14439c43e1120c
Sana Javed <sanajaved917@gmail.com>
2023-04-02 11:46:22 +0100

    String operations

---
+++ day2.py
@@ -0,0 +1,50 @@
+firstletter='Sana Javed'
+lastletter='Zeeshan'
+space=''
+fullname=firstletter+space+lastletter
+age=21
+print(fullname)
+print(len(fullname))
+print('i am thankful to Allah')
+
+python_libraries = ['Django', 'Flask', 'NumPy', 'Matplotlib','Pandas']
+formated_string = 'The following are python libraries:%s' % (python_libraries)
+print(formated_string) # "The following are python libraries:['Django', 'Flask', 'NumPy', 'Matplotlib','Pandas']
+print('i am %s and my age is %d' %(firstletter,age))
+print('{} + {}= {}'.format(2,3,2+3))
+name='python'
+print(name[0])
+
+a='python'
+b='is'
+c='programming'
+dd=print('{} {} {}'.format(a,b,c))
+company="coding for all"
+print(len(company))
+print(company.upper())
+company1="coding For All"
+print(company1.lower())
+a="coding for All "
+print(a.capitalize())
+print(a.caseswap())
+a="coding for All "
+print(a.capitalize())
+print(a.swapcase())
+print(a[0:6])
+print(a.find('coding'))
+sub='coding'
+b=a.index(sub)
+#question36
+a=8
+b=6
+print('{} + {} ={}'.format(a,b,a+b))
+print('{} - {} ={}'.format(a,b,a-b))
+a='my name is sana. '
+print('I hope every one enjoying the python challenge.\nDo you ?') # line break
+print('Days\tTopics\tExercises')
+print('Day 1\t3\t5')
+print('Day 2\t3\t5')
+print('Day 3\t3\t5')
+print('Day 4\t3\t5')
+
+
